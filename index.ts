86360052111b70f96b126ec18aca01ef14fc3d86
Jim Jiang <jim@lotlab.org>
2022-01-03 11:17:46 +0800

    添加配置文件，修改配置文件的引用方式

---
+++ index.ts
@@ -0,0 +1,35 @@
+export default [
+    // Lotlab
+    require("./lot60ble.json"),
+    require("./lot60lite.json"),
+    require("./g84-4100.json"),
+    require("./ergoconn_left.json"),
+    require("./ergoconn_right.json"),
+    // Glab
+    require("./lotpad.json"),
+    require("./gt-ble60.json"),
+    require("./gt-ble60-e.json"),
+    require("./gt-ble60-f.json"),
+    require("./gt-planck.json"),
+    require("./newhope64.json"),
+    require("./farad69.json"),
+    require("./farad69-b.json"),
+    require("./farad69-c.json"),
+    require("./omega45.json"),
+    require("./omega45-e.json"),
+    require("./omega50.json"),
+    require("./omega50-b.json"),
+    require("./omega64.json"),
+    require("./omega84.json"),
+    require("./gt-pad.json"),
+    require("./omega40.json"),
+    require("./gt-volta9.json"),
+    // HHKB
+    require("./hhkb_ble.json"),
+    require("./htkb_hypertoxic.json"),
+    // AliceFafa
+    require("./alice-go.json"),
+    // AMX
+    require("./amx_2hkb60.json")
+
+];
