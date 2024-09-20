b5b779a9681901375416667dbbeff35c2c040793
Jim Jiang <jim@lotlab.org>
2024-09-20 20:27:17 +0800

    refact: 更新到vue3

--- index.ts
+++ index.ts
@@ -1,48 +1,96 @@
+// Lotlab
+import lotlab_lot60ble from "./lotlab/lot60ble.json";
+import lotlab_lot60lite from "./lotlab/lot60lite.json";
+import lotlab_g84_4100 from "./lotlab/g84-4100.json";
+import lotlab_ergoconn_left from "./lotlab/ergoconn_left.json";
+import lotlab_ergoconn_right from "./lotlab/ergoconn_right.json";
+import lotlab_lotpad from "./lotlab/lotpad.json";
+// Glab
+import glab_gt_ble60_d from "./glab/gt-ble60-d.json";
+import glab_gt_ble60_e from "./glab/gt-ble60-e.json";
+import glab_gt_ble60_f from "./glab/gt-ble60-f.json";
+import glab_gt_planck_a from "./glab/gt-planck-a.json";
+import glab_gt_planck_b from "./glab/gt-planck-b.json";
+import glab_newhope64 from "./glab/newhope64.json";
+import glab_newhope64_new from "./glab/newhope64-new.json";
+import glab_farad69_a from "./glab/farad69-a.json";
+import glab_farad69_b from "./glab/farad69-b.json";
+import glab_farad69_c from "./glab/farad69-c.json";
+import glab_omega45 from "./glab/omega45.json";
+import glab_omega45_e from "./glab/omega45-e.json";
+import glab_omega50 from "./glab/omega50.json";
+import glab_omega50_b from "./glab/omega50-b.json";
+import glab_omega64 from "./glab/omega64.json";
+import glab_omega84 from "./glab/omega84.json";
+import glab_gt_pad from "./glab/gt-pad.json";
+import glab_omega40 from "./glab/omega40.json";
+import glab_gt_volta9 from "./glab/gt-volta9.json";
+import glab_hal67 from "./glab/hal67.json";
+// HHKB
+import htkb_hhkb_ble from "./htkb/hhkb_ble.json";
+import htkb_htkb_hypertoxic from "./htkb/htkb_hypertoxic.json";
+import htkb_htkb_ble from "./htkb/htkb_ble.json";
+// AliceFafa
+import youngfafa_alice_go from "./youngfafa/alice-go.json";
+// AMX
+import amx_amx_2hkb60 from "./amx/amx_2hkb60.json";
+import amx_amx_ibm66 from "./amx/amx_ibm66.json";
+import amx_amx_amx40 from "./amx/amx_amx40.json";
+import amx_amx_gx64 from "./amx/amx_gx64.json";
+import amx_amx_amx60 from "./amx/amx_amx60.json";
+// Clover
+import clover_clover_40 from "./clover/clover-40.json";
+// Kawvin
+import Kawvin_BLE102 from "./Kawvin/BLE102.json";
+// Zero
+import Zero_FX_980 from "./Zero/FX-980.json";
+
 export default [
     // Lotlab
-    require("./lotlab/lot60ble.json"),
-    require("./lotlab/lot60lite.json"),
-    require("./lotlab/g84-4100.json"),
-    require("./lotlab/ergoconn_left.json"),
-    require("./lotlab/ergoconn_right.json"),
-    require("./lotlab/lotpad.json"),
+    lotlab_lot60ble,
+    lotlab_lot60lite,
+    lotlab_g84_4100,
+    lotlab_ergoconn_left,
+    lotlab_ergoconn_right,
+    lotlab_lotpad,
+
     // Glab
-    require("./glab/gt-ble60-d.json"),
-    require("./glab/gt-ble60-e.json"),
-    require("./glab/gt-ble60-f.json"),
-    require("./glab/gt-planck-a.json"),
-    require("./glab/gt-planck-b.json"),
-    require("./glab/newhope64.json"),
-    require("./glab/newhope64-new.json"),
-    require("./glab/farad69-a.json"),
-    require("./glab/farad69-b.json"),
-    require("./glab/farad69-c.json"),
-    require("./glab/omega45.json"),
-    require("./glab/omega45-e.json"),
-    require("./glab/omega50.json"),
-    require("./glab/omega50-b.json"),
-    require("./glab/omega64.json"),
-    require("./glab/omega84.json"),
-    require("./glab/gt-pad.json"),
-    require("./glab/omega40.json"),
-    require("./glab/gt-volta9.json"),
-    require("./glab/hal67.json"),
+    glab_gt_ble60_d,
+    glab_gt_ble60_e,
+    glab_gt_ble60_f,
+    glab_gt_planck_a,
+    glab_gt_planck_b,
+    glab_newhope64,
+    glab_newhope64_new,
+    glab_farad69_a,
+    glab_farad69_b,
+    glab_farad69_c,
+    glab_omega45,
+    glab_omega45_e,
+    glab_omega50,
+    glab_omega50_b,
+    glab_omega64,
+    glab_omega84,
+    glab_gt_pad,
+    glab_omega40,
+    glab_gt_volta9,
+    glab_hal67,
     // HHKB
-    require("./htkb/hhkb_ble.json"),
-    require("./htkb/htkb_hypertoxic.json"),
-    require("./htkb/htkb_ble.json"),
+    htkb_hhkb_ble,
+    htkb_htkb_hypertoxic,
+    htkb_htkb_ble,
     // AliceFafa
-    require("./youngfafa/alice-go.json"),
+    youngfafa_alice_go,
     // AMX
-    require("./amx/amx_2hkb60.json"),
-    require("./amx/amx_ibm66.json"),
-    require("./amx/amx_amx40.json"),
-    require("./amx/amx_gx64.json"),
-    require("./amx/amx_amx60.json"),
+    amx_amx_2hkb60,
+    amx_amx_ibm66,
+    amx_amx_amx40,
+    amx_amx_gx64,
+    amx_amx_amx60,
     // Clover
-    require("./clover/clover-40.json"),
+    clover_clover_40,
     // Kawvin
-    require("./Kawvin/BLE102.json"),
+    Kawvin_BLE102,
     // Zero
-    require("./Zero/FX-980.json"),
+    Zero_FX_980,
 ];
